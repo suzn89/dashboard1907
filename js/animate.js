ba20ff9af12da30f1bc7498367075ebb6b7b38e4
suzn89 <ssostory@gmail.com>
2019-07-25 11:49:32 +0900

    대상자조회 완료

--- js/animate.js
+++ js/animate.js
@@ -17,21 +17,35 @@ $(function() {
   var wrapHeight = $('.contents').height();
   var searchHeight = $('.search__box').height();
   $('.content__box').css('height', wrapHeight-searchHeight-45);
+  $('.content__box > .listwrap').css('height', wrapHeight-searchHeight-140);
+  $('.content__box > .listwrap').css('overflow-y', 'scroll');
   $(window).resize(function(){
     var wrapHeight = $('.contents').height();
     var searchHeight = $('.search__box').height();
     $('.content__box').css('height', wrapHeight-searchHeight-45);
+    $('.content__box > .listwrap').css('height', wrapHeight-searchHeight-140);
+    $('.content__box > .listwrap').css('overflow-y', 'scroll');
   });
 });
 
 
 // 팝업 열기,닫기
-$(function(){
+$(function(){ // edit modal
   $('.modal').hide();
   $('.modal__edit').on('click',function(){
-    $('.modal').fadeIn();
+    $('.edit_modal').fadeIn();
   });
   $('.popup--close, .btnarea > .close').on('click',function(){
-    $('.modal').fadeOut();
+    $('.edit_modal').fadeOut();
+  });  
+});
+
+$(function(){ // mapping modal
+  $('.modal').hide();
+  $('.modal__mapping').on('click',function(){
+    $('.mapping_modal').fadeIn();
+  });
+  $('.popup--close, .btnarea > .close').on('click',function(){
+    $('.mapping_modal').fadeOut();
   });  
 });
